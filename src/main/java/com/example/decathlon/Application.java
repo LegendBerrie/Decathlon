a90adb9e38f5245248591eaee7edb70516138d61
Moe <mohammed.kaldi@outlook.com>
2026-02-26 22:10:08 +0100

    test.com

--- src/main/java/com/example/decathlon/Application.java
+++ src/main/java/com/example/decathlon/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    //hejsan alla
+    //H
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
