243df4217f5482100e2036b115d64ba8e7e0bfa5
LegendBerrie <lovisablomster@gmail.com>
2026-03-11 21:44:01 +0100

    Test Lovisa

--- src/test/java/stepDefinitions/WebGuiSteps.java
+++ src/test/java/stepDefinitions/WebGuiSteps.java
@@ -1,10 +1,12 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.*;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.Select;
 
 public class WebGuiSteps {
 
@@ -31,4 +33,28 @@ public class WebGuiSteps {
 
         }
     }
+
+    @When("I enter the competitors name {string}")
+    public void iEnterTheCompetitorsName(String name) {
+        driver.findElement(By.id("name2")).sendKeys(name);
+    }
+
+    @And("I choose the event {string}")
+    public void iChooseTheEvent(String event) {
+        Select eventDropDown = new Select(driver.findElement(By.id("event")));
+        eventDropDown.selectByVisibleText(event);
+    }
+
+    @And("I enter the result {double}")
+    public void iEnterTheResult(double result) {
+        driver.findElement(By.id("raw")).sendKeys(String.valueOf(result));
+    }
+
+    @Then("the system shall display the score {int}")
+    public void theSystemShallDisplayTheScore(int expectedScore) {
+        String totalScore = driver.findElement(By.id("")).getText();
+        int actualScore = Integer.parseInt(totalScore);
+        Assertions.assertEquals(expectedScore, actualScore);
+    }
+
 }
